d69516d518ca8f1391dd065f923102a544e7539d
KennHuang <kennhung@users.noreply.github.com>
2024-08-18 23:51:59 +0800

    [add] GitHubPackage publications

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,6 +3,8 @@ import com.google.protobuf.gradle.*
 plugins {
     `java-library`
 
+    `maven-publish`
+
     // Protobug plugin
     id("com.google.protobuf") version "0.9.4"
 }
@@ -25,24 +27,25 @@ dependencies {
 }
 
 protobuf {
-  protoc {
-    // Download pre-built protoc binary from Maven repository.
-    artifact = "com.google.protobuf:protoc:3.8.0"
-  }
+    protoc {
+        // Download pre-built protoc binary from Maven repository.
+        artifact = "com.google.protobuf:protoc:3.8.0"
+    }
 
-  plugins {
-    id("grpc") {
-      // Download pre-built protoc-gen-grpc-java binary from Maven repository.
-      artifact = "io.grpc:protoc-gen-grpc-java:1.24.0"
+    plugins {
+        id("grpc") {
+            // Download pre-built protoc-gen-grpc-java binary from Maven repository.
+            artifact = "io.grpc:protoc-gen-grpc-java:1.24.0"
+        }
     }
-  }
-  generateProtoTasks {
-    ofSourceSet("main").forEach {
-      it.plugins {
-        id("grpc") { }
-      }
+
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                id("grpc") { }
+            }
+        }
     }
-  }
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -55,4 +58,28 @@ java {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.kennhung"
+            artifactId = "gobgp-grpc-java"
+            version = "3.29.0"
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kennhung/gobgp-grpc-java")
+
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
