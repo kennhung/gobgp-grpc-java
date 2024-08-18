be4fcf685542266a9ff40fe1a7d97e45b60f3e4d
KennHuang <kennhung@users.noreply.github.com>
2024-08-18 23:35:57 +0800

    [modify] change rootProject.name in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "gobgp-grpc-java-client"
+rootProject.name = "gobgp-grpc-java"
 include("lib")
